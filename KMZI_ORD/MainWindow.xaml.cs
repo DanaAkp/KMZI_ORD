805b3915c8ef31bb43e000007d9db0d46cb80298
Danagul Akpaeva <aidanagul10@gmail.com>
2019-03-18 23:07:11 +0300

    Доделать алгоритм вычисления порядка многочлена: проверить формулу вычисления е и должны ли быть сравнимы с 1 по модулю многочлена или нет

--- KMZI_ORD/MainWindow.xaml.cs
+++ KMZI_ORD/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KMZI_lib;
 
 namespace KMZI_ORD
 {
@@ -29,14 +30,61 @@ namespace KMZI_ORD
         {
             try
             {
-                tblResult.Text = "";
-                int p = int.Parse(tbMod.Text.Trim());
+                tbRes.Text = "";
+                int mod = int.Parse(tbMod.Text.Trim());
 
-                int[] Koeff1 = GetVector(tbKoeff.Text.Trim().Split());//старший коэфф стоит последний, то есть его индекс обозначает его степень
-                int[] Koeff2 = GetVector(tbKoeff2.Text.Trim().Split());
+                int[] Koeff = GetVector(tbKoeff.Text.Trim().Split());//старший коэфф стоит последний, то есть его индекс обозначает его степень
+                tbRes.Text += "" + GetOrd_f(Koeff, mod);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
 
-                tblResult.Text = DivisionPolinom(Koeff1, Koeff2, p);
-            }catch(Exception ex) { MessageBox.Show(ex.Message); }
+        private int GetOrd_f(int[] K,int mod)
+        {
+            int d = (int)Math.Pow(mod, K.Length - 1) - 1;
+            List<int> forNOK = new List<int>();
+            Dictionary<int, int> f = Factorization(d);
+            foreach(KeyValuePair<int,int> x in f)
+            {
+                for (int i = 0; i < x.Value; i++)
+                {
+                    int[] k1 = new int[d / (int)Math.Pow(x.Key, i) + 1];
+                    if (ModPolinom(k1, K, mod) == new int[1] { 1 })
+                    {
+                        forNOK.Add((int)Math.Pow(x.Key, x.Value));
+                        break;
+                    }
+                }
+            }
+            int e = d;
+            if (forNOK.Count == 1) e = forNOK[0];
+            if (forNOK.Count > 1)
+            {
+                e = NOK(forNOK[0], forNOK[1]);
+                for (int i = 2; i < forNOK.Count; i++) e = NOK(e, forNOK[i]);
+            }
+            return e;
+        }
+
+        private Dictionary<int, int> Factorization(int num)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for(int i = 2; i <= num; i++)
+            {
+                if (Class1.CheckForSimplicity(i) && num%i==0)
+                {
+                    int c = 0;
+                    while (num % i == 0)  { num /= i; c++; }
+                    dic.Add(i, c);
+                }
+            }
+
+            return dic;
+        }
+        private int NOK(int m,int n)
+        {
+            return m * n / (int)Class1.NOD(m, n);
         }
         private int[] GetVector(string[] s)
         {
@@ -48,7 +96,7 @@ namespace KMZI_ORD
             Array.Reverse(ls, 0, ls.Length);
             return ls;
         }
-        private string DivisionPolinom(int[] K1,int[] K2, int mod)
+        private int[] DivisionPolinom(int[] K1,int[] K2, int mod)
         {
           //  int deg = K1.Length - 1;
             int[] result = new int[(K1.Length - 1) - (K2.Length - 1) + 1];
@@ -72,17 +120,53 @@ namespace KMZI_ORD
                     K1 = buf;//deg = i + K2.Length - 1;
                 }
             }
-            string s = "Коэффициенты многочлена: ";
             Array.Reverse(result);
             Array.Reverse(K1);
-            for (int i = 0; i < result.Length; i++)
+            //string s = "Коэффициенты многочлена: ";
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    s += result[i] + " ";
+            //}
+            //s += "\nКоэффициенты остатка: ";
+            //for (int i = 0; i < K1.Length; i++)
+            //    s += K1[i] + " ";
+            return result;
+        }
+        private int[] ModPolinom(int[] K1, int[] K2, int mod)
+        {
+            //  int deg = K1.Length - 1;
+            int[] result = new int[(K1.Length - 1) - (K2.Length - 1) + 1];
+            if (K2.Length > K1.Length) throw new Exception("Степень делимого многочлена должна быть выше делителя!");
+            for (int i = K1.Length - 1; i >= 0; i--)
             {
-                s += result[i] + " ";
+                int[] buf = new int[K1.Length];
+                if (i >= K2.Length - 1)
+                {
+                    result[i - (K2.Length - 1)] = K1[i];//разобраться с индексом, тк в рес должно быть меньше i
+
+                    for (int k = 0; k < K2.Length; k++)
+                    {
+                        buf[i - (K2.Length - 1) + k] = result[i - (K2.Length - 1)] * K2[k];
+                    }
+                    for (int j = K1.Length - 1; j >= 0; j--)
+                    {
+                        buf[j] = K1[j] - buf[j];
+                        buf[j] = numOnMod(buf[j], mod);
+                    }
+                    K1 = buf;//deg = i + K2.Length - 1;
+                }
             }
-            s += "\nКоэффициенты остатка: ";
-            for (int i = 0; i < K1.Length; i++)
-                s += K1[i] + " ";
-            return s;
+            Array.Reverse(result);
+            Array.Reverse(K1);
+            //string s = "Коэффициенты многочлена: ";
+            //for (int i = 0; i < result.Length; i++)
+            //{
+            //    s += result[i] + " ";
+            //}
+            //s += "\nКоэффициенты остатка: ";
+            //for (int i = 0; i < K1.Length; i++)
+            //    s += K1[i] + " ";
+            return K1;
         }
         private int numOnMod(int num,int mod)
         {
